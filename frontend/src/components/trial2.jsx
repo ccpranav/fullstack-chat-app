cb2db7de1d37753e9647c6c88a98058edcd18aa8
Pranav <ccpranav@gmail.com>
2025-10-23 11:31:50 +0530

    Trying to adjust navbar fixed size(old nav)

--- frontend/src/components/trial2.jsx
+++ frontend/src/components/trial2.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Dither from "./ui/Dither";
 
 const trial2 = () => {
-  return <div></div>;
+  return (
+    <div style={{ width: "100%", height: "600px", position: "relative" }}>
+      <Dither
+        waveColor={[0.5, 0.5, 0.5]}
+        disableAnimation={false}
+        enableMouseInteraction={true}
+        mouseRadius={0.3}
+        colorNum={4}
+        waveAmplitude={0.3}
+        waveFrequency={3}
+        waveSpeed={0.05}
+      />
+    </div>
+  );
 };
 
 export default trial2;
